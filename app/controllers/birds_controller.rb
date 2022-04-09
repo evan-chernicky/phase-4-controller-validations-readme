b1ebed212c3205eae4697b3c5a943df8bd5cce24
Evan Chernicky <echernicky@gmail.com>
2022-04-09 12:20:00 -0400

    commit tree 136cac7c8dbfbfc07bc05519dd94a9b6b741f51f

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -10,7 +10,9 @@ class BirdsController < ApplicationController
   # POST /birds
   def create
     bird = Bird.create(bird_params)
-    render json: bird, status: :created
+      render json: bird, status: :created
+    rescue ActiveRecord::RecordInvalid => invalid
+      render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   # GET /birds/:id
@@ -24,6 +26,8 @@ class BirdsController < ApplicationController
     bird = find_bird
     bird.update(bird_params)
     render json: bird
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   # DELETE /birds/:id
@@ -34,7 +38,11 @@ class BirdsController < ApplicationController
   end
 
   private
-
+  
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  end
+  
   def find_bird
     Bird.find(params[:id])
   end
